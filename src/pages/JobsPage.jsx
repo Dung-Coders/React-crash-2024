54bb185cf747b5e94f5aae2124a6c87a7983a002
Dung-Coders <vandunghp1985@gmail.com>
2024-06-17 17:53:05 +0700

    React-crash-course-17/06/2024

---
+++ src/pages/JobsPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import JobListings from '../components/JobListings'
+
+function JobsPage() {
+  return (
+    <>
+        <JobListings/>
+    </>
+  )
+}
+
+export default JobsPage
